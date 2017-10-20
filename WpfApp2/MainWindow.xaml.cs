b33e04078d733682ee1dd534e214a5d476439e7b
yanzer1 <yannis.serrand@gmail.com>
2017-10-20 13:47:04 +0200

    WPF with DB connection

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    
+
+    public partial class MainWindow : Window
+    {
+        #region MySqlConnection
+        MySqlConnection conn = new MySqlConnection("Server=localhost;userid=root;password=root;Database=yay");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+        #region Bind Data to DataGrid
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM customer", conn);
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds, "LoadDataBinding");
+                DataGrid.DataContext = ds;
+            }
+            catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        #endregion
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO customer(name,lastName,Adress) VALUES(Ivan, DesSandwichs, 15 rue de la Guillotière)");
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds, "LoadDataBinding");
+                DataGrid.DataContext = ds;
+            }
+            catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ WpfApp2/SQLConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WpfApp2
+{
+    class SQLConnection
+    {
+
+    }
+}
